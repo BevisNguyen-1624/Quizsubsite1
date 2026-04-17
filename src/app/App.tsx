13e28e3d2ba965863cd739c92118aa4e9da009f1
BevisNguyen-1624 <binhnh@yody.vn>
2026-04-17 19:43:49 +0700

    background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import { QuizQuestion } from "./components/QuizQuestion";
 import { QuizResults } from "./components/QuizResults";
 import { PersonalIdEntry } from "./components/PersonalIdEntry";
 import type { CSSProperties } from "react";
-import bgImage from "C:/Users/Administrator/Bình - git/Quizsubsite1/src/imports/background111.jpg";
+import {bgImage} from "C:/Users/Administrator/Bình - git/Quizsubsite1/src/imports/background111.jpg";
 
 type Phase = "intro" | "id" | "quiz" | "results";
 
