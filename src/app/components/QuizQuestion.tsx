694aa95d84696469ef58bde7532a0213cb4ea463
BevisNguyen-1624 <binhnh@yody.vn>
2026-04-17 20:41:39 +0700

    image-5.jpeg

--- src/app/components/QuizQuestion.tsx
+++ src/app/components/QuizQuestion.tsx
@@ -100,6 +100,16 @@ export function QuizQuestion({
 
       {/* Question */}
       <div className="mb-6">
+        {question.image && (
+          <div className="flex justify-center my-4">
+            <img
+              src={question.image}
+              alt="question"
+              className="max-h-56 rounded-xl object-contain"
+              style={{ background: "#fff" }}
+            />
+          </div>
+        )}
         <h2
           className="text-gray-800 leading-relaxed"
           style={{ fontSize: "1.08rem", fontWeight: 700 }}
