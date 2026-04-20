a5f4d582c0ca6eff1d59a5139cf6cbe7632fb8ac
BevisNguyen-1624 <binhnh@yody.vn>
2026-04-20 22:57:10 +0700

    pass

--- api/save-result.js
+++ api/save-result.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://binhnh_db_user:awFFIPTQHHstW1yL@cluster0.dmu7njj.mongodb.net/mydb?appName=Cluster0";
+const uri = process.env.MONGODB_URI || "mongodb+srv://binhnh_db_user:bcpPs765VSApWYc9@cluster0.dmu7njj.mongodb.net/mydb?appName=Cluster0";
 
 let cached = global.mongoose;
 
